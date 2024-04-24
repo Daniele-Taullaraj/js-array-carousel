7085031b6ad671c62b09959e8cb7a3806c94eca4
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-04-24 15:30:21 +0200

    M3

--- readme.md
+++ readme.md
@@ -3,9 +3,13 @@
 -Aggiungo un'po di css per gestire il layout e creo anche 2 classi apposite per gestire il d-none e il d-block delle img, dov'è possibile utilizzo bootstrap (button,ecc..)
 
 JS
-MILESTONE1
+MILESTONE2
 -prendo tutti gli elementi con la classe slide
 -controllo a partire dal primo elemento che contiene la classe slide se è presente la classe active
 -in tal caso la rimuovo
     -se l'indice è minore della lunghezza (dell'array) allora aggiungi la classe active all'elemento con la classe slide seguente e aumenta l'indice di 1
     -se invece è uguale aggiungi la classe active allo zeresimo elemento e resetta l'indice a 0
+
+
+MILESTONE3
+-aggiungo una funzione al click del bottone next che mi contiene le stesse istruzioni di M2
--- scripts.js
+++ scripts.js
@@ -1,18 +1,40 @@
+// MILESTONE 2
+// let slides = document.getElementsByClassName("slide");
+// console.log(slides)
+// let i = 0;
+
+// if (slides[i].classList.contains("active")) {
+//     slides[i].classList.remove("active");
+//     if (i == (slides.length - 1)) {
+//         slides[0].classList.add("active");
+//         i = 0;
+//     } else {
+//         slides[i + 1].classList.add("active");
+//         i += 1;
+//     }
+// }
+
+
+
+// MILESTONE 3
+
 let slides = document.getElementsByClassName("slide");
 console.log(slides)
 let i = 0;
+const next = document.querySelector(".nextBtn");
+next.addEventListener("click", function () {
 
-if (slides[i].classList.contains("active")) {
-    slides[i].classList.remove("active");
-    if (i == (slides.length - 1)) {
-        slides[0].classList.add("active");
-        i = 0;
-    } else {
-        slides[i + 1].classList.add("active");
-        i += 1;
+    if (slides[i].classList.contains("active")) {
+        slides[i].classList.remove("active");
+        if (i == (slides.length - 1)) {
+            slides[0].classList.add("active");
+            i = 0;
+        } else {
+            slides[i + 1].classList.add("active");
+            i += 1;
+        }
     }
-}
-
+})
 
 
 
